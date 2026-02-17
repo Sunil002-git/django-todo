178d340a8f3d66edafcc8e8eafa5d65d4a0d2433
Sunil002-git <sunilmarri886@gmail.com>
2026-02-17 21:11:05 +0530

    form submitted at 31

--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from todos.models import Task
 def home(request):
-    tasks = Task.objects.filter(is_completed = False)
+    tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
     context = {
         'tasks' : tasks
     }
